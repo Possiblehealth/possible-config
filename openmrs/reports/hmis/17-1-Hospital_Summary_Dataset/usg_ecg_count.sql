c80b0f79094b9f24fa787d675255979b119237d3
suruchee <suruchee11@gmail.com>
2020-04-14 16:40:34 +0545

    usg query and mapping updated

--- openmrs/reports/hmis/17-1-Hospital_Summary_Dataset/usg_ecg_count.sql
+++ openmrs/reports/hmis/17-1-Hospital_Summary_Dataset/usg_ecg_count.sql
@@ -1,63 +1,39 @@
-SELECT *
-FROM (SELECT
-        IF(shortName.name IS NULL, base.concept_name, shortName.name) AS concept_name,
-        ifnull(sum(final.male + final.other + final.female ),0)                    AS total
-      FROM
-        (SELECT
-           rag.name AS age_group,
-           cn.concept_id,
-           cn.name  AS concept_name,
-           rag.sort_order,
-           va.value_reference
-         FROM reporting_age_group rag, concept_name cn, (SELECT DISTINCT value_reference
-                                                         FROM visit_attribute va
-                                                         WHERE va.visit_attribute_id IS NOT NULL ) va
-         WHERE rag.report_group_name = 'All Ages' AND cn.name IN ('ANC-USG placenta position','ECG note') AND
-               cn.concept_name_type = 'FULLY_SPECIFIED') base
+SELECT 
+    SUM(final.usg) AS USG,
+    SUM(final.ecg) AS ECG
+FROM
+(SELECT 
+	SUM(IF(first_concept.name IN ('ANC-USG placenta position','USG-Cleaned before use'), 1, 0)) AS usg,
+	SUM(IF(first_concept.name = 'ECG note', 1, 0)) AS ecg,
+    COUNT(DISTINCT (first_concept.person_id)) AS count
+FROM
+    (SELECT 
+		question_concept_name.name AS answer_name
+    FROM
+        concept c
+    INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
+    INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
+	AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+	AND question_concept_name.voided IS FALSE
+    WHERE
+        question_concept_name.name IN ('ANC-USG placenta position','USG-Cleaned before use', 'ECG note')
+    ORDER BY answer_name DESC) first_answers
         LEFT OUTER JOIN
-        (SELECT
-           concept_name,
-           concept_id,
-           age_group,
-           visit_type,
-           sum(female) AS female,
-           sum(male)   AS male,
-           sum(other)  AS other,
-           sort_order
-         FROM (SELECT
-                 cn.name                                         AS concept_name,
-                 cn.concept_id                                   AS concept_id,
-                 SUM(IF(p.gender = 'F', 1, 0)) AS female,
-                 SUM(IF(p.gender = 'M', 1, 0)) AS male,
-                 SUM(IF(p.gender = 'O', 1, 0)) AS other,
-                 va.value_reference                              AS visit_type,
-                 rag.name                                        AS age_group,
-                 p.person_id                                     AS person_id,
-                 rag.sort_order                                  AS sort_order
-               FROM obs obs
-                 INNER JOIN concept_name cn
-                   ON obs.concept_id = cn.concept_id AND cn.concept_name_type = 'FULLY_SPECIFIED' AND obs.voided = 0 AND
-                      cn.voided = 0
-                 INNER JOIN encounter e ON obs.encounter_id = e.encounter_id
-                 INNER JOIN visit v ON v.visit_id = e.visit_id
-                 INNER JOIN visit_attribute va ON va.visit_id = v.visit_id
-                 INNER JOIN visit_attribute_type vat
-                   ON vat.visit_attribute_type_id = va.attribute_type_id AND vat.name = 'Visit Status'
-                 INNER JOIN person p ON p.person_id = obs.person_id
-                 INNER JOIN reporting_age_group rag ON DATE(v.date_stopped) BETWEEN (DATE_ADD(
-                     DATE_ADD(birthdate, INTERVAL rag.min_years YEAR), INTERVAL rag.min_days DAY)) AND (DATE_ADD(
-                     DATE_ADD(birthdate, INTERVAL rag.max_years YEAR), INTERVAL rag.max_days DAY))
-                                                       AND rag.report_group_name = 'All Ages'
-               WHERE
-                 cn.name IN ('ANC-USG placenta position','ECG note') AND cast(v.date_stopped AS DATE) BETWEEN '#startDate#' AND '#endDate#' AND
-                 obs.voided IS FALSE
-               GROUP BY cn.name, cn.concept_id, va.value_reference, rag.name, rag.sort_order, p.person_id) result
-         GROUP BY concept_name, age_group, visit_type
-         ORDER BY concept_name, result.sort_order) final
-          ON final.age_group = base.age_group AND final.concept_id = base.concept_id AND
-             final.visit_type = base.value_reference
-        LEFT OUTER JOIN concept_name shortName
-          ON shortName.concept_id = base.concept_id AND shortName.concept_name_type = 'SHORT' AND shortName.voided = 0
-          
-          group by concept_name
-      ORDER BY base.sort_order ASC) AS result;
+    (SELECT DISTINCT
+        o1.person_id as person_id,
+		cn1.concept_id AS question,
+		cn1.name AS name
+    FROM
+        obs o1
+        
+    INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
+        AND cn1.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn1.name IN ('ANC-USG placenta position','USG-Cleaned before use','ECG note')
+        AND o1.voided = 0
+        AND cn1.voided = 0
+    INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
+    INNER JOIN person p1 ON o1.person_id = p1.person_id
+    WHERE
+        DATE(e.encounter_datetime) BETWEEN DATE('#startDate#') AND DATE('#endDate#')
+           ) first_concept ON first_concept.name = first_answers.answer_name
+GROUP BY first_answers.answer_name)final;
