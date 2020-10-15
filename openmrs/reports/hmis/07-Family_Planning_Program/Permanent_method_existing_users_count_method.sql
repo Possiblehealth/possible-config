0db2c10f9689a273f614d3b7ffef8a4f4b2acc26
suruchee <suruchee11@gmail.com>
2020-10-15 13:15:04 +0545

    family planning permanent menthod query changed

--- openmrs/reports/hmis/07-Family_Planning_Program/Permanent_method_existing_users_count_method.sql
+++ openmrs/reports/hmis/07-Family_Planning_Program/Permanent_method_existing_users_count_method.sql
@@ -1,112 +1,67 @@
 SELECT 
-  SUM(new_continue.new_female)+ SUM(new_continue.prev_female)+ SUM(new_continue.tubectomy) AS continous_female, 
-  SUM(new_continue.new_male)+ SUM(new_continue.prev_male) AS continous_male 
-FROM 
-  (
-    SELECT 
-      sum(final.new_female) as 'new_female', 
-      sum(final.new_male) as 'new_male', 
-      sum(final.prev_female) as 'prev_female', 
-      sum(final.prev_male) as 'prev_male', 
-      sum(final.tubectomy) as 'tubectomy' 
-    FROM 
-      -- --------------------------- No. of new permanent--------------------------
-      (
-        SELECT 
-          IFNULL(
-            SUM(
-              CASE WHEN new_month.gender = 'F' THEN 1 ELSE 0 END
-            ), 
-            0
-          ) AS new_female, 
-          IFNULL(
-            SUM(
-              CASE WHEN new_month.gender = 'M' THEN 1 ELSE 0 END
-            ), 
-            0
-          ) AS new_male, 
-          0 as prev_female, 
-          0 as prev_male, 
-          0 as tubectomy 
-        FROM 
-          (
-            SELECT 
-              DISTINCT o1.person_id, 
-              p1.gender, 
-              cn2.concept_id AS answer, 
-              cn1.concept_id AS question 
-            FROM 
-              obs o1 
-              INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id 
-              AND cn1.concept_name_type = 'FULLY_SPECIFIED' 
-              AND cn1.name in (
-                'FRH-Long acting and permanent method', 
-                'FRH-New method chosen'
-              ) 
-              AND o1.voided = 0 
-              AND cn1.voided = 0 
-              INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id 
-              AND cn2.concept_name_type = 'FULLY_SPECIFIED' 
-              And cn2.name in ('Vasectomy', 'Minilap') 
-              AND cn2.voided = 0 
-              INNER JOIN encounter e ON o1.encounter_id = e.encounter_id 
-              INNER JOIN visit v1 ON v1.visit_id = e.visit_id 
-              INNER JOIN person p1 ON o1.person_id = p1.person_id 
-            WHERE 
-              DATE(e.encounter_datetime) BETWEEN DATE('#startDate#') AND DATE('#endDate#')
-          ) new_month 
-                
--- --------------------------- No. of old permanent--------------------------
-
-        UNION ALL 
-        SELECT 
-          0, 
-          0, 
-          prev_female, 
-          prev_male, 
-          0 
-        FROM 
-          (
-            select 
-              contiFemale_user as prev_female, 
-              contiMale_user as prev_male 
-            from 
-              familyPlanning
-          ) as pre_month 
--- --------------------------- No. of tubectomy-------------------------
-
-        UNION ALL 
-        SELECT 
-          0, 
-          0, 
-          0, 
-          0, 
-          SUM(tubectomy_patient) as tubectomy 
-        FROM 
-          (
-            SELECT 
-              COUNT(
-                DISTINCT(o1.person_id)
-              ) as tubectomy_patient 
-            FROM 
-              obs o1 
-              INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id 
-              AND cn1.concept_name_type = 'FULLY_SPECIFIED' 
-              AND cn1.name in (
-                'Discharge-Contraceptive Chosen'
-              ) 
-              AND o1.voided = 0 
-              AND cn1.voided = 0 
-              INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id 
-              AND cn2.concept_name_type = 'FULLY_SPECIFIED' 
-              And cn2.name in ('Tubectomy') 
-              AND cn2.voided = 0 
-              INNER JOIN encounter e ON o1.encounter_id = e.encounter_id 
-              INNER JOIN visit v1 ON v1.visit_id = e.visit_id 
-              INNER JOIN person p1 ON o1.person_id = p1.person_id 
-            WHERE 
-              DATE(e.encounter_datetime) BETWEEN DATE('#startDate#') AND DATE('#endDate#')
-          ) as tubectomy_patient
-      ) final
-  ) new_continue;
-
+    gender.gender,    
+    count(distinct(first_concept.person_id)) as existing_users
+FROM
+    (SELECT 
+        ca.answer_concept AS answer,
+            IFNULL(answer_concept_short_name.name, answer_concept_fully_specified_name.name) AS answer_name
+    FROM
+        concept c
+    INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
+    INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
+        AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+        AND question_concept_name.voided IS FALSE
+    INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
+    INNER JOIN concept_name answer_concept_fully_specified_name ON ca.answer_concept = answer_concept_fully_specified_name.concept_id
+        AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED'
+         And answer_concept_fully_specified_name.name in ('Vasectomy','Mini-lap')
+        AND answer_concept_fully_specified_name.voided
+        IS FALSE
+    LEFT JOIN concept_name answer_concept_short_name ON ca.answer_concept = answer_concept_short_name.concept_id
+        AND answer_concept_short_name.concept_name_type = 'SHORT'
+        AND answer_concept_short_name.voided
+        IS FALSE
+    WHERE
+        question_concept_name.name IN ('FRH-Long acting and permanent method')
+            AND cd.name = 'Coded' 
+    ORDER BY answer_name DESC) first_answers
+    
+    
+  INNER JOIN (SELECT 'M' AS gender
+              UNION SELECT 'F' AS gender) gender
+              
+              
+        LEFT JOIN
+    (SELECT DISTINCT
+    cn2.concept_id AS answer,
+    cn2.name AS method_name,
+    o.person_id,
+	p1.gender
+FROM
+    obs o
+        INNER JOIN
+    concept_name cn ON o.concept_id = cn.concept_id
+        AND cn.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn.name IN ('FRH-short acting method provided' , 'FRH-Long acting and permanent method',
+        'FRH-New method chosen',
+        'FRH-Existing continued')
+        AND o.voided = 0
+        AND cn.voided = 0
+        INNER JOIN
+    concept_name cn2 ON o.value_coded = cn2.concept_id
+        AND cn2.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn2.name IN ('Vasectomy' , 'Mini-lap')
+        AND cn2.voided = 0
+        INNER JOIN
+    encounter e ON o.encounter_id = e.encounter_id
+        INNER JOIN
+    visit v1 ON v1.visit_id = e.visit_id
+        INNER JOIN
+    person p1 ON o.person_id = p1.person_id
+WHERE
+    o.obs_datetime <= DATE('#endDate#')) first_concept 
+            ON first_concept.answer = first_answers.answer
+            and first_concept.gender = gender.gender
+GROUP BY gender
+order by gender;
+					
